46e4622e11204ac840eed4167f45688e0ae9486e
Marawanelhaggar1 <Marawanmagdy91@gmail.com>
2023-03-22 08:35:25 +0200

    header

--- clientSide/gulpFile.js
+++ clientSide/gulpFile.js
@@ -2,6 +2,15 @@ const gulp = require("gulp");
 const gulpSass = require("gulp-sass");
 const sass = gulpSass(require("sass"));
 
-gulp.task("sass", async () => {
-  gulp.src("src/components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
+// gulp.task("sass", async () => {
+//   gulp.src("src/components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
+// });
+
+gulp.task("watch", async () => {
+  gulp.watch("src/components/**/*.scss", () => {
+    gulp
+      .src("src/components/**/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("src/css"));
+  });
 });
---
+++ clientSide/src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../../css/Footer/footer.css";
+const Footer = () => {
+  return <footer>&copy; COPYWRIGHT Marwan ElHaggar 2023</footer>;
+};
+
+export default Footer;
